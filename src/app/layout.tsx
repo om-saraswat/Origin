247e2ae14d3439752c0ed97c61eb9981db5c7aa0
Om Saraswat <sharmaom27022004@gmail.com>
2026-08-13 16:05:41 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,13 +51,9 @@ export const metadata: Metadata = {
   applicationName: 'Origin Inclusive Special School',
   category: 'education',
   icons: {
-    icon: [
-      { url: '/favicon-48.png', sizes: '48x48', type: 'image/png' },
-      { url: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/icon-512.png', sizes: '512x512', type: 'image/png' },
-    ],
-    apple: [{ url: '/apple-icon.png', sizes: '180x180', type: 'image/png' }],
-    shortcut: ['/favicon-48.png'],
+    icon: [{ url: '/favicoon.png', type: 'image/png' }],
+    apple: [{ url: '/favicoon.png', type: 'image/png' }],
+    shortcut: ['/favicoon.png'],
   },
   alternates: {
     canonical: '/',
